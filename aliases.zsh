5ba248dab07e15e4353f13f8cbe611914017b43b
imTHAI <imthai@leet.la>
2024-10-24 10:31:25 +0200

    modify some aliases

--- aliases.zsh
+++ aliases.zsh
@@ -5,17 +5,21 @@ alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="eza -l --group-directories-first"
 alias ls="eza --group-directories-first"
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
-alias sync_photoslib="rsync -av --exclude='.DS_Store' --delete /Volumes/Mac\ HD\ Extra/Photos\ Library.photoslibrary coruscant:/mnt/user/backups/"
-alias sync_calibre="rsync -av --exclude='.DS_Store' --delete /Volumes/Mac\ HD\ Extra/Librairie\ Calibre coruscant:/mnt/user/media/books/"
+alias sync_photoslib="rsync -a --exclude='.DS_Store' --delete --info=name0 --info=copy2,del2,flist0,stats2,misc0,remove \
+                     /Volumes/Mac\ HD\ Extra/Photos\ Library.photoslibrary coruscant:/mnt/user/backups/"
+alias sync_calibre="rsync -a --exclude='.DS_Store' --delete --info=name0 --info=copy2,del2,flist0,stats2,misc0,remove\
+                     /Volumes/Mac\ HD\ Extra/Librairie\ Calibre coruscant:/mnt/user/media/books/"
 
 # Backup my local bin folder to coruscant and to iCloud
-alias sync_bin="rsync -av --exclude='.DS_Store' --delete ~/Applications/bin coruscant:/mnt/user/backups/ && \
-                rsync -av --exclude='.DS_Store' --delete ~/Applications/bin /Users/pbear/Library/Mobile\ Documents/com~apple~CloudDocs/Backups/ "
+alias sync_bin="rsync -av --exclude='.DS_Store' --delete --info=name0 --info=copy2,del2,flist0,stats2,misc0,remove2 ~/Applications/bin coruscant:/mnt/user/backups/ && \
+                rsync -av --exclude='.DS_Store' --delete \
+                --info=name0 --info=copy2,del2,flist0,stats2,misc0,remove2 ~/Applications/bin /Users/pbear/Library/Mobile\ Documents/com~apple~CloudDocs/Backups/ "
 
 
 # Backup some folders from my homedir on coruscant to iCloud
-alias sync_homedir="for src in coruscant:/mnt/user/homedir-pbear/scripts-NEVERDELETE coruscant:/mnt/user/homedir-pbear/docker ; \
-                    do rsync -av --exclude='.DS_Store' --delete $src /Users/pbear/Library/Mobile\ Documents/com~apple~CloudDocs/Backups/homedir-pbear/ ; done"
+alias sync_homedir="for src in coruscant:/mnt/user/homedir-pbear/scripts-NEVERDELETE coruscant:/mnt/user/homedir-pbear/docker ;\
+                    do rsync -a --exclude='.DS_Store' --delete --info=name0 --info=copy2,del2,flist0,stats2,misc0,remove2\
+                    $src /Users/pbear/Library/Mobile\ Documents/com~apple~CloudDocs/Backups/homedir-pbear/ ; done"
 
 alias sed=gsed
 
