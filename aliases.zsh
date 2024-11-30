bc6c76a872ec724b5920c0c5a7826a50a6dfbb28
imTHAI <imthai@leet.la>
2024-11-30 11:03:45 +0100

    add aliases

--- aliases.zsh
+++ aliases.zsh
@@ -7,7 +7,6 @@ alias ls="eza --group-directories-first"
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias  dl="aria2c -x4 --dir=/Users/pbear/Downloads"
 alias notarize="codesign --deep -f -s -"
-
 alias sync_photoslib="rsync -vah --exclude='.DS_Store' --delete \
                      /Volumes/Mac\ HD\ Extra/Photos\ Library.photoslibrary coruscant:/mnt/user/backups/"
 alias sync_calibre="rsync -vah --exclude='.DS_Store' --delete \
@@ -52,7 +51,16 @@ function rm {
     done
 }
 
+function heic2jpg {
+    local input_file="$1"
+    local output_file="${input_file%.*}.jpg"
 
+    # Utilisation du chemin absolu
+    input_path="$(realpath "$input_file")"
+    output_path="$(dirname "$input_path")/${input_file%.*}.jpg"
+
+    sips -s format jpeg "$input_path" --out "$output_path"
+}
 
 alias sed=gsed
 
